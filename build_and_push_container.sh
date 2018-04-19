9421290ce1fff65c2cba5ae938e042a8019e59b7
Felix Schmitz <felix@darumatic.com>
2018-04-19 16:21:48 +1000

    N/A: Update to version 3.0.

--- build_and_push_container.sh
+++ build_and_push_container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 env GOOS=linux GOARCH=amd64 go build &&
-docker build . -t darumatic/nexus-cleaner:latest &&
-docker push darumatic/nexus-cleaner:latest
+docker build . -t darumatic/nexus-cleaner:3.0 &&
+docker push darumatic/nexus-cleaner:3.0
